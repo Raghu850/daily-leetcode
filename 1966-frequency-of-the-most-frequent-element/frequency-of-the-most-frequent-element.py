661e4941cb94633bb7ecf7fa15be7ae1d9d69dd2
Adabala Raghu Ram <raghuram829729@gmail.com>
2026-07-28 10:38:06 +0530

    Time: 176 ms (96.38%) | Memory: 30.7 MB (87.78%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
@@ -3,11 +3,9 @@ class Solution:
         nums.sort()
         l=0
         s=0
-        ans=1
         for r in range(len(nums)):
             s+=nums[r]
-            while nums[r]*(r-l+1)-s>k:
+            if nums[r]*(r-l+1)-s>k:
                 s-=nums[l]
                 l+=1
-            ans=max(ans,r-l+1)
-        return ans
+        return  len(nums)-l
