c498e731cd1217f52618fba544d13cacd76de28e
Adabala Raghu Ram <raghuram829729@gmail.com>
2026-08-01 10:42:51 +0530

    Time: 7 ms (57.22%) | Memory: 21.2 MB (18.85%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -9,10 +9,4 @@ class Solution:
                 cnt+=1
             else:
                 cnt-=1
-        cnt1=0
-        for i in range(len(nums)):
-            if nums[i]==ele:
-                cnt1+=1
-        if cnt1>len(nums)//2:
-            return ele
-        return -1
+        return ele
