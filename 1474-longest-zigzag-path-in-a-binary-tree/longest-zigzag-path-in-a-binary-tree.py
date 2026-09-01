83b357403060d4eadf4f9f997d7663d8d805647b
Adabala Raghu Ram <raghuram829729@gmail.com>
2026-09-01 23:02:28 +0530

    Time: 55 ms (41.34%) | Memory: 38.3 MB (60.92%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.py
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.py
@@ -1,29 +1,13 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
-    def longestZigZag(self, root: TreeNode) -> int:
-        self.ans = 0
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        return max(self.dfs(root.left, True, 0), self.dfs(root.right, False, 0))
 
-        def dfs(node, goLeft, length):
-            if not node:
-                return
-            # update max answer so far
-            self.ans = max(self.ans, length)
-
-            if goLeft:
-                # if going left now, next must be right
-                dfs(node.left, False, length + 1)
-                # OR restart fresh on right
-                dfs(node.right, True, 1)
-            else:
-                dfs(node.right, True, length + 1)
-                dfs(node.left, False, 1)
-
-        dfs(root, True, 0)   # assume starting left
-        dfs(root, False, 0)  # assume starting right
-        return self.ans
+    def dfs(self, root, isLeft, length):
+        if not root:
+            return length
+        
+        if isLeft:
+            return max(self.dfs(root.right, False, length+1), self.dfs(root.left, True, 0))
+        else:
+            return max(self.dfs(root.left, True, length+1), self.dfs(root.right, False, 0))
+      
