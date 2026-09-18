b16d5a76b94b41e1ad28f9e9eaa465989c917b12
Adabala Raghu Ram <raghuram829729@gmail.com>
2026-09-18 14:51:03 +0530

    Time: 150 ms (96.18%) | Memory: 20.7 MB (13.16%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,15 +1,27 @@
 class Solution:
+    def timeTaken(self, piles: List[int], speed: int) -> int:
+        totalTime = 0
+        for n in piles:
+            totalTime += ceil(n/speed)
+        
+        return totalTime
+
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l=1
-        r=max(piles)
-        while l<=r:
-            mid=(l+r)//2
-            ch=0
-            for b in piles:
-                ch+=ceil(b/mid)
-            if ch<=h:
-                res=mid
-                r=mid-1
+        # The range shuld be between min(piles) and max(piles)
+        low = 1
+        high = max(piles)
+        ans = 1e9
+
+        while(low <= high):
+            mid = low + (high-low)//2
+
+            # If within limits look for a lower number
+            if self.timeTaken(piles, mid) <= h:
+                ans = mid
+                high = mid - 1
+            
+            # Look for a higher number
             else:
-                l=mid+1
-        return res
+                low = mid + 1
+        
+        return ans
